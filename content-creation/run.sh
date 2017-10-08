c4bead402362074ce0eae61ff444bc738dbd0279
Michel Grootjans <michel.grootjans@gmail.com>
2017-10-08 15:27:11 +0200

    Optimized startup time

--- content-creation/run.sh
+++ content-creation/run.sh
@@ -1,4 +1,4 @@
-rm tmp/pids/server.pid
 bundle install
-bundle exec rake db:create db:migrate
+bundle exec rake db:create db:migrate &
+rm tmp/pids/server.pid
 bundle exec rails server -p 3000 -b '0.0.0.0'
